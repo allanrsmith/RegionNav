d5fd53b153c96107ec0e182cacbe34dd598c2ecb
Allan Smith <allan.smith@procore.com>
2015-12-18 09:00:21 -0800

    Inital commit

---
+++ RegionNav/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Prism.Unity;
+using RegionNav.Views;
+using System.Windows;
+
+namespace RegionNav
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterTypeForNavigation<ChildView>();
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+    }
+}
---
+++ RegionNav/ViewModels/ParentViewViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace RegionNav.ViewModels
+{
+    public class ParentViewViewModel : BindableBase
+    {
+        public ParentViewViewModel()
+        {
+            // Not using DI due to legacy beast
+            _regionManager = ServiceLocator.Current.GetInstance<IRegionManager>();
+            LoadCommand = new DelegateCommand(DoNavigate);
+        }
+
+        IRegionManager _regionManager;
+
+        private void DoNavigate()
+        {
+            _regionManager.RequestNavigate("ChildRegion", "ChildView");
+        }
+
+        public DelegateCommand LoadCommand { get; set; }
+    }
+}
